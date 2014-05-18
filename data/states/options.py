19ae4022deecd72285b8157f6a6061559f20e15e
metulburr <micahpage911@yahoo.com>
2014-05-18 16:42:12 -0400

    polished options menu

--- data/states/options.py
+++ data/states/options.py
@@ -23,27 +23,35 @@ class Options(tools.States):
         self.menu_item_bg_w = 200
         self.menu_item_bg_h = 25
         
+        self.setup_buttons()
+        
+    def setup_buttons(self):
         button_config = {
             "clicked_font_color" : (0,0,0),
             "hover_font_color"   : (205,195, 0),
             'font'               : tools.Font.load('impact.ttf', 18),
             'font_color'         : (255,255,255),
             'border_color'       : (0,0,0),
+            'clicked_color'      : (255,255,255),
+            'hover_color'        : (0,0,130),
         }
-        c = (0,0,0) #clicked color for color buttons
         self.fullscreen_button = button.Button((10,10,105,25),(0,0,100), 
-            self.toggle_fullscreen, text='Fullscreen', clicked_color=(255,255,255), 
-            hover_color=(0,0,130), **button_config
-        )
+            self.toggle_fullscreen, text='Fullscreen', **button_config)
         self.window_button = button.Button((10,40,105,25),(0,0,100), 
-            lambda:self.set_window((800, 600)), text='(800, 600)', clicked_color=(255,255,255), 
-            hover_color=(0,0,130), **button_config
-        )
-        self.default_button = button.Button((10,70,105,25),(0,0,100), 
-            lambda:self.set_window(self.default_screensize), text=str(self.default_screensize), clicked_color=(255,255,255), 
-            hover_color=(0,0,130), **button_config
-        )
-        self.buttons = [self.fullscreen_button, self.window_button, self.default_button]
+            lambda:self.set_window((640,360)), text='640x360', **button_config)
+        self.window2_button = button.Button((10,70,105,25),(0,0,100), 
+            lambda:self.set_window((854,480)), text='854x480', **button_config)
+        self.window3_button = button.Button((10,100,105,25),(0,0,100), 
+            lambda:self.set_window((1280,720)), text='1280x720', **button_config)
+        self.default_button = button.Button((10,130,105,25),(0,0,100), 
+            lambda:self.set_window(self.default_screensize), text=str(self.default_screensize[0])+'x'+str(self.default_screensize[1]), **button_config)
+        self.buttons = [self.fullscreen_button, self.default_button, self.window_button, 
+            self.window2_button, self.window3_button]
+        
+        spacer = 30
+        from_top = 100
+        for i, b in enumerate(self.buttons):
+            b.rect.center = (self.screen_rect.x + self.screen_rect.width//2, self.screen_rect.y + i * spacer + from_top)
         
     def toggle_fullscreen(self):
         self.change_res = 'fullscreen'
